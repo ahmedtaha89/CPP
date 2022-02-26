a25d0dac8c20d20df625de229ea48a4a45e1ca38
ahmedtaha89 <95527363+ahmedtaha89@users.noreply.github.com>
2022-02-26 02:25:33 +0200

    TEST CPP

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   
+
+	int area;
+	int width;
+	int leght;
+	cout << "enter the value of wight and length : \n";
+
+	cin >> width >> leght;
+	area = width * leght;
+	cout << " the area  = " << area << "\n";
+
+
+
+
+
+	return 0;
+}
